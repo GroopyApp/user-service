8fe82a3a2f8416516ecf285df4aa6c68b9e65eb9
alessiodanna <alessio.danna.94@gmail.com>
2023-01-19 15:17:43 +0100

    moved mapper to infrastructure layer

--- src/main/java/app/groopy/userservice/infrastructure/services/ElasticsearchUserService.java
+++ src/main/java/app/groopy/userservice/infrastructure/services/ElasticsearchUserService.java
@@ -1,17 +1,16 @@
 package app.groopy.userservice.infrastructure.services;
 
-import app.groopy.userservice.application.mapper.ApplicationMapper;
 import app.groopy.userservice.domain.models.common.UserDetails;
+import app.groopy.userservice.infrastructure.mapper.InfrastructureMapper;
 import app.groopy.userservice.infrastructure.repository.ESUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ElasticsearchUserService {
 
     @Autowired
-    private ApplicationMapper mapper;
+    private InfrastructureMapper mapper;
 
     @Autowired
     private ESUserRepository esUserRepository;
